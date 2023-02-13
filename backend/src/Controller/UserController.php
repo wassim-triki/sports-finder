ed2e9cc2cc13445f27c10e3b1edd1258953e097c
Wassim Triki <wsmtriki@gmail.com>
2023-02-13 01:39:15 +0100

    add intital docker-compose.yml configuration

---
+++ backend/src/Controller/UserController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/api/users", name="get_all_users")
+     */
+    public function getAllUsers(ManagerRegistry $doctrine): JsonResponse
+    {
+        $users = $doctrine->getRepository(User::class)->findAll();
+        die();
+        // dd($users);
+        // return $this->json([
+        //     'users' => $users,
+        // ]);
+    }
+}
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const resp = await axios.get('http://localhost:8080/api/users');
+        setData(resp.data);
+      } catch (error) {
+        throw error;
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {loading && 'loading...'}
+      {data && !loading && JSON.stringify(data)}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
